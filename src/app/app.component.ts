f6a6b1295c9e4ad4824baf946a5961f42b7a808b
Nicolas Molina Monroy <hi@nicobytes.com>
2020-09-30 15:12:04 -0400

    Work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+import Swiper from 'swiper/bundle';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   title = 'ng-swiper';
+  swiper: Swiper;
+
+  ngAfterContentInit(): void {
+    this.swiper = new Swiper('.swiper-container', {
+      updateOnWindowResize: true,
+      loop: true
+    });
+  }
+
+  next(): void {
+    this.swiper.slideNext();
+  }
+
+  prev(): void {
+    this.swiper.slidePrev();
+  }
 }
